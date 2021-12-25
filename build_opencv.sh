b2223897d106323560885e772c37dfe1833867ca
NL647 <76846739+NL647@users.noreply.github.com>
2021-12-25 16:24:32 +0100

    Update build_opencv.sh

--- build_opencv.sh
+++ build_opencv.sh
@@ -111,7 +111,7 @@ configure () {
         -D CUDA_ARCH_BIN=5.3,6.2,7.2
         -D CUDA_ARCH_PTX=
         -D CUDA_FAST_MATH=ON
-        -D CUDNN_VERSION='8.0'
+        -D CUDNN_VERSION='8.2.1.32'
         -D EIGEN_INCLUDE_PATH=/usr/include/eigen3 
         -D ENABLE_NEON=ON
         -D OPENCV_DNN_CUDA=ON
